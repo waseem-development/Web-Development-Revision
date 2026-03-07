2b412c8b1846e10adc56375969429556f15840a8
Hafiz Waseem Ahmed <hafizwaseemahmed2002@gmail.com>
2026-03-07 18:13:03 +0500

    began another todo with Redux Toolkit

---
+++ React/11_react_redux_tool_kit/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <p className='mx-auto my-44 h-45 text-white bg-teal-600 p-6 text-center'>
+      Hey there
+    </p>
+  )
+}
+
+export default App
---
+++ React/11_react_redux_tool_kit/src/features/todo/todoSlice.js
@@ -0,0 +1,39 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [{ id: 1, text: "Hello World", completed: false }], // Added completed property
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload.text,
+        completed: false, // Add completed property to new todos
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    updateTodo: (state, action) => {
+      const { id, text } = action.payload;
+      const existingTodo = state.todos.find((todo) => todo.id === id);
+      if (existingTodo) {
+        existingTodo.text = text;
+      }
+    },
+    toggleTodo: (state, action) => { // Added missing parameters
+      const todo = state.todos.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+  },
+});
+
+export const { addTodo, removeTodo, updateTodo, toggleTodo } = todoSlice.actions;
+export default todoSlice.reducer;
